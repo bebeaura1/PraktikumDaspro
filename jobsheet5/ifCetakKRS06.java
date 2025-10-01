2c761d8ef61c8e241942ced7b64b6d9ce89a3696
Aura Bintang Aprilian <auraaprilian0@gmail.com>
2025-10-01 12:27:29 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS06.java
+++ jobsheet5/ifCetakKRS06.java
@@ -5,16 +5,12 @@ public class ifCetakKRS06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("--- Cetak KRS SIAKAD---");
+        System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA"); 
-        } else {
-            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
-        }
+        String x = (uktLunas) ? "Pembayaran UKT terverivikasi" : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu.";
+        System.out.println(x); 
 
     }
 }
